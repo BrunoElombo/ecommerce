1b1b624ca7430c7f6788cdb7da0918fa0e09c91e
brunoelombo <brunoelombo@gmail.com>
2025-06-25 17:26:44 +0100

    fix Loading color prop

--- src/app/modules/ShopSection.tsx
+++ src/app/modules/ShopSection.tsx
@@ -33,14 +33,13 @@ const ShopSection = () => {
         <h2 className='text-6xl md:text-8xl flex flex-wrap'>Products</h2>
         <div className='flex flex-col w-full md:flex-row md:items-center gap-4 my-8 flex-wrap'>
           {
-            products ? 
+            !(products.length > 0) ? 
               <div className='flex items-center justify-center gap-4 text-light'>
                 <ShoppingBagIcon className='h-12 text-white/50' />
                 <h4 className='text-white text-4xl'>No product found</h4>
               </div>
             :
-            products.map((product:any) => <ProductCard 
-                                                id={product.id}
+            products?.map((product:any) => <ProductCard 
                                                 key={product.slug}
                                                 title={product.title}
                                                 description={product.description}
@@ -48,7 +47,6 @@ const ShopSection = () => {
                                                 price={product.price}
                                                 stock={product.stock}
                                                 slug={product.slug}
-                                                category={product.category}
                                               />)
           }
         </div>
